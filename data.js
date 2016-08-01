ce63ee0780220f4366b21096ec4190f1b9df5079
Maggie <margaritakarlsson@yahoo.se>
2016-08-01 20:59:10 +0200

    Bettter solution for function: whatShouldIHaveToday

--- data.js
+++ data.js
@@ -1,10 +1,20 @@
 var data = [ /* Månaderna */
-    ["Pankaka 15 min",
-     "Receptet gäller för 8 st. <br> Ingredienser 2½ dl vetemjöl 6 dl Arla® mjölk 3 ägg ½ tsk salt 2 msk Arla® Svenskt Smör",
-     "Gör så här: Vispa ut mjölet i hälften av mjölken till en slät smet. Vispa i resterande mjölk, ägg och salt. Låt smeten svälla ca 10 min. Smält smör i en stekpanna och häll ner i smeten. Grädda tunna pannkakor."],
-    ["Isad grön gazpacho med kungsräkor",
-     "400 g frysta kungsräkor eller räkspett (400g motsvarar 4 vannameiräkspett) 4 träspett 1 vitlöksklyfta 1 gurka 1/2 grön chili 1 avokado 1 silverlök 1 kruka persilja 1 kruka basilika 65 g babyspenat 3 dl vatten 2 msk rödvinsvinäger 1 dl olivolja salt och peppar",
-     "Så här gör du Close Bra att veta när du ändrar antalet portioner Detta recept är gjort för 4 portioner. När du ändrar antalet portioner så ändras också mängden i ingredienserna. I vissa fall kan detta resultera i att tillagningstiden behöver justeras. Tina räkorna och skala dem. Lägg träspetten i vatten ca 10 minuter. Skala och hacka vitlöken. Skär gurkan i bitar. Dela, kärna ur och skiva chilin och avokadon. Skala och hacka löken. Mixa grönsakerna med örter, spenat och vatten. Smaksätt med vinäger, olivolja, salt och peppar. Trä upp räkorna på spetten och grilla dem ca 2 minuter på varje sida. Servera gazpachon med räkorna. För alla Fri från gluten, laktos, mjölkprotein och ägg. Tips Lägg krasse eller ätbara blommor i ett istärningsfack, fyll på med vatten och frys. Lägg i soppan."],
+    ["pankaka 15 min",
+     "Receptet gäller för 8 st. <br> Ingredienser <br> 2½ dl vetemjöl <br> 6 dl mjölk <br> 3 ägg ½ tsk salt <br> 2 msk Svenskt Smör",
+     "Gör så här: Vispa ut mjölet i hälften av mjölken till en slät smet. Vispa i resterande mjölk, ägg och salt. <br>" +
+     "Låt smeten svälla ca 10 min. Smält smör i en stekpanna och häll ner i smeten. Grädda tunna pannkakor."],
+    ["isad grön gazpacho med kungsräkor 45 min",
+     "400 g frysta kungsräkor eller räkspett (400g motsvarar 4 vannameiräkspett) <br> 4 träspett <br> 1 vitlöksklyfta <br> 1 " +
+     "gurka <br> 1/2 grön chili <br> 1 avokado <br> 1 silverlök <br> 1 kruka persilja <br> 1 kruka basilika <br> 65 g babyspenat " +
+     " <br> 3 dl vatten <br> 2 msk rödvinsvinäger <br> 1 dl olivolja salt och peppar",
+     "Så här gör du Close Bra att veta när du ändrar antalet portioner Detta recept är gjort för 4 portioner. <br>" +
+     "När du ändrar antalet portioner så ändras också mängden i ingredienserna. I vissa fall kan detta resultera <br>" +
+     "i att tillagningstiden behöver justeras. Tina räkorna och skala dem. Lägg träspetten i vatten ca 10 minuter. <br>" +
+     "Skala och hacka vitlöken. Skär gurkan i bitar. Dela, kärna ur och skiva chilin och avokadon. Skala och <br>" +
+     "hacka löken. Mixa grönsakerna med örter, spenat och vatten. Smaksätt med vinäger, olivolja, salt och peppar. <br>" +
+     "Trä upp räkorna på spetten och grilla dem ca 2 minuter på varje sida. Servera gazpachon med räkorna. För <br>" +
+     "alla Fri från gluten, laktos, mjölkprotein och ägg. Tips Lägg krasse eller ätbara blommor i ett istärningsfack, <br>" +
+     "fyll på med vatten och frys. Lägg i soppan."],
     ["c", "3", "Puff"],
     ["d", "4", "Linjal"],
     ["e", "5", "Orangutang"]
